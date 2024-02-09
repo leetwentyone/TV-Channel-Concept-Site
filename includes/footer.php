5ebcae0d2f152612372e5b9e1e2c7aa457a5d4fd
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-02-09 13:45:58 +0000

    updated template

--- includes/footer.php
+++ includes/footer.php
@@ -3,32 +3,32 @@
 
     <div class="footer-links">
       <ul>
-        <div class="col-heading">Column Heading</div>
-        <li><a href="#">Footer Link One</a></li>
-        <li><a href="#">Footer Link Two</a></li>
-        <li><a href="#">Footer Link Three</a></li>
-        <li><a href="#">Footer Link Four</a></li>
+        <div class="col-heading">Sectors</div>
+        <li><a href="#">Corporate</a></li>
+        <li><a href="#">Personal</a></li>
+        <li><a href="#">Medical</a></li>
+        <li><a href="#">Hospitality</a></li>
       </ul>
       <ul>
-        <div class="col-heading">Column Heading</div>
-        <li><a href="#">Footer Link Five</a></li>
-        <li><a href="#">Footer Link Six</a></li>
-        <li><a href="#">Footer Link Seven</a></li>
-        <li><a href="#">Footer Link Eight</a></li>
-        <li><a href="#">Footer Link Nine</a></li>
-        <li><a href="#">Footer Link Ten</a></li>
+        <div class="col-heading">More about us</div>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Careers</a></li>
+        <li><a href="#">Contact us</a></li>
+        <li><a href="#">Offices</a></li>
+        <li><a href="#">Help &amp; Advice</a></li>
       </ul>
       <ul>
-        <div class="col-heading">Column Heading</div>
-        <li><a href="#">Footer Link Eleven</a></li>
-        <li><a href="#">Footer Link Twelve</a></li>
-        <li><a href="#">Footer Link Thirteen</a></li>
-        <li><a href="#">Footer Link Fourteen</a></li>
+        <div class="col-heading">Other</div>
+        <li><a href="#">Privacy policy</a></li>
+        <li><a href="#">Terms &amp; Conditions</a></li>
+        <li><a href="#">Subscribe</a></li>
+        <li><a href="#">Your data</a></li>
       </ul>
     </div>
 
     <div class="copyright">
-      <p>&copy;This is the copyright text, you could add text and <a href="#">links</a> within this section.</p>
+      <p>&copy; This is the copyright text, you could add text and <a href="#">links</a> within this section.</p>
     </div>
 
   </div>
--- includes/header.php
+++ includes/header.php
@@ -1,34 +1,45 @@
 <header class="site-header">
   <div class="header-inner">
 
-    Header Inner
+    <a href="#" class="search-toggle"><img src="images/search.png"></a>
+    <a href="tel:0000000000">0000 000 000</a>
 
   </div>
 
   <nav class="primary">
     <ul>
-      <li class="has-children"><a href="#">Home</a>
+      <li class="has-children"><a href="#">The Company</a>
         <div class="submenu">
           
           <div class="col">
-            Column One
+            <ul>
+              <li><a href="#">Our services</a></li>
+              <li><a href="#">Sectors</a></li>
+              <li><a href="#">What to expect</a></li>
+              <li><a href="#">How we can help</a></li>
+              <li><a href="#">More about us</a></li>
+              <li><a href="#">Our locations</a></li>
+              <li><a href="#">Get in touch</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Careers</a></li>
+            </ul>
           </div>
 
           <div class="col">
-            Column Two
+            Column Two    
           </div>
 
-          <div class="col-slim">
+          <div class="col">
             Column Three
           </div>
 
         </div>
       </li>
-      <li><a href="#">Link One</a></li>
-      <li><a href="#">Link Two</a></li>
-      <li><a href="#">Link Three</a></li>
-      <li><a href="#">Link Four</a></li>
-      <li><a href="#">Link Five</a></li>
+      <li><a href="#">Business</a></li>
+      <li><a href="#">Sectors</a></li>
+      <li><a href="#">Personal</a></li>
+      <li><a href="#">What we do</a></li>
+      <li><a href="#">Get in touch</a></li>
     </ul>
   </nav>
 
