08faa249f837aa5013cb51c6536190672d1a65b1
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-02-14 10:19:51 +0000

    updated template

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,7 @@
     </div> 
 
     <a href="#" class="search-toggle"><img src="images/search.png"><img src="images/close.png"></a>
+    <a href="mailto:contact@email.co.uk">contact@email.co.uk</a>
     <a href="tel:0000000000">0000 000 000</a>   
 
   </div>
@@ -42,10 +43,114 @@
 
         </div>
       </li>
-      <li><a href="#">Business</a></li>
-      <li><a href="#">Sectors</a></li>
-      <li><a href="#">Personal</a></li>
-      <li><a href="#">What we do</a></li>
+      <li class="has-children"><a href="#">Business</a>
+        <div class="submenu">
+          
+          <div class="col">
+            <ul>
+              <li><a href="#">Our services</a></li>
+              <li><a href="#">Sectors</a></li>
+              <li><a href="#">What to expect</a></li>
+              <li><a href="#">How we can help</a></li>
+              <li><a href="#">More about us</a></li>
+              <li><a href="#">Our locations</a></li>
+              <li><a href="#">Get in touch</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Careers</a></li>
+            </ul>
+          </div>
+
+          <div class="col">
+            Column Two    
+          </div>
+
+          <div class="col">
+            Column Three
+          </div>
+
+        </div>
+      </li>
+      <li class="has-children"><a href="#">Sectors</a>
+        <div class="submenu">
+          
+          <div class="col">
+            <ul>
+              <li><a href="#">Our services</a></li>
+              <li><a href="#">Sectors</a></li>
+              <li><a href="#">What to expect</a></li>
+              <li><a href="#">How we can help</a></li>
+              <li><a href="#">More about us</a></li>
+              <li><a href="#">Our locations</a></li>
+              <li><a href="#">Get in touch</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Careers</a></li>
+            </ul>
+          </div>
+
+          <div class="col">
+            Column Two    
+          </div>
+
+          <div class="col">
+            Column Three
+          </div>
+
+        </div>
+      </li>
+      <li class="has-children"><a href="#">Personal</a>
+        <div class="submenu">
+          
+          <div class="col">
+            <ul>
+              <li><a href="#">Our services</a></li>
+              <li><a href="#">Sectors</a></li>
+              <li><a href="#">What to expect</a></li>
+              <li><a href="#">How we can help</a></li>
+              <li><a href="#">More about us</a></li>
+              <li><a href="#">Our locations</a></li>
+              <li><a href="#">Get in touch</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Careers</a></li>
+            </ul>
+          </div>
+
+          <div class="col">
+            Column Two    
+          </div>
+
+          <div class="col">
+            Column Three
+          </div>
+
+        </div>
+      </li>
+      <li class="has-children"><a href="#">What we do</a>
+        <div class="submenu">
+          
+          <div class="col">
+            <ul>
+              <li><a href="#">Our services</a></li>
+              <li><a href="#">Sectors</a></li>
+              <li><a href="#">What to expect</a></li>
+              <li><a href="#">How we can help</a></li>
+              <li><a href="#">More about us</a></li>
+              <li><a href="#">Our locations</a></li>
+              <li><a href="#">Get in touch</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Careers</a></li>
+            </ul>
+          </div>
+
+          <div class="col">
+            Column Two    
+          </div>
+
+          <div class="col">
+            Column Three
+          </div>
+
+        </div>
+      </li>
       <li><a href="#">Get in touch</a></li>
     </ul>
   </nav>
